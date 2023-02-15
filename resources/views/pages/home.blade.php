dee420f57a1ff7980673b480537fd4d45fcd85d7
albyschianni <alberto.schianni@gmail.com>
2023-02-15 01:34:01 +0100

    front end home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,36 @@
 
 @section('content')
 
-    <h1>Hello World</h1>
+    <h1>People</h1>
+
+    <ul>
+        @foreach ($people as $person)
+            <li>
+                <h3>{{ $person -> name }}</h3>
+                Email: {{ $person -> mail }}
+                <br>
+                Phone: {{ $person -> personDetail -> phone }}
+                <br>
+                Address: {{ $person -> personDetail -> address }}
+                <hr>
+
+                @if ($person -> posts -> count() > 0)
+                    <h3>Posts: {{ $person -> posts -> count() }} </h3>
+                
+                    <ul>
+                        @foreach ($person -> posts as $post)
+                            <li>
+                                <b>{{ $post -> title }}</b> <br>
+                                <div>{{ $post -> body }}</div>
+                            </li>
+                        @endforeach
+                    </ul>
+                @endif
+                
+                
+            </li>
+        @endforeach
+    </ul>
+
     
 @endsection
